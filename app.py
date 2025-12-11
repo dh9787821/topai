cb43d8326682846c84adc466da313fe38a6cfb91
dh9787821 <dh97878211@gmail.com>
2025-12-11 14:01:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from PIL import Image
+import pandas as pd
 
-st.title('도와줘!')
-st.write('this is for deplying at streamlit')
+# Sidebar contents
+st.sidebar.title("KNU Top AI")
+img = Image.open("logo.jpg")
+st.sidebar.image(img)
