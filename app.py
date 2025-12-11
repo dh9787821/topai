919cb9174d17b1d70189f14b616ca587b5dd5c1e
dh9787821 <dh97878211@gmail.com>
2025-12-11 14:10:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import streamlit as st
 from PIL import Image
 import pandas as pd
+from datetime import datetime
+
+
 
 # Sidebar contents
 st.sidebar.title("KNU Top AI")
 img = Image.open("logo.jpg")
-st.sidebar.image(img, width=10)
+st.sidebar.image(img, width=40)
 
 # //////////////////////////////////////////////////////////
 
